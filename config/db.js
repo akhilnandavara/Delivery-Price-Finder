9f6c3d268a34b3351c71d7296b6371079fd55775
akhilnandavara <akhiln1108@gmail.com>
2024-03-28 15:09:16 +0530

    Update database and swagger configuration

--- config/db.js
+++ config/db.js
@@ -1,24 +1,26 @@
 const { Sequelize } = require('sequelize');
-
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
-    dialectOptions: {
-      ssl: false, // Disable SSL
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, /* This line allows you to connect to a PostgreSQL
+        database that uses a self-signed certificate.
+       If your PostgreSQL database uses a valid SSL certificate, you should set this to true. */
     },
   },
 });
 
 sequelize
-  .sync()
+  .authenticate()
   .then(() => {
-    console.log('Database connected');
+    console.log('Connection has been established successfully.');
   })
   .catch((err) => {
-    console.log(err);
+    console.error('Unable to connect to the database:', err);
   });
 
 module.exports = sequelize;
